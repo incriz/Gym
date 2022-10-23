d9c84435a54ba28f96de821b9bd7e976235c4ba2
incriz <viamore777@gmail.com>
2022-10-23 21:06:20 +0300

    final version

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,87 @@
-import React from 'react'
-import './Header.css'
-import Logo from '../../assets/logo.png'
+import { useState } from "react";
+import "./Header.css";
+import Logo from "../../assets/logo.png";
+import Bars from "../../assets/bars.png";
+import { Link } from "react-scroll";
 
 const Header = () => {
+  const mobile = window.innerWidth <= 768 ? true : false;
+  const [menuOpend, setMenuOpend] = useState(false);
+
   return (
     <div className="header">
-        <img src={Logo} alt="" className='logo'/>
-
-        <ul className='header-menu'>
-          <li>Home</li>
-          <li>Programs</li>
-          <li>Why us</li>
-          <li>Plans</li>
-          <li>Testimonials</li>
+      <img src={Logo} alt="" className="logo" />
+      {menuOpend === false && mobile === true ? (
+        <div
+          style={{
+            backgroundColor: "var(--appColor)",
+            padding: "0.5rem",
+            borderRadius: "5px",
+          }}
+          onClick={() => setMenuOpend(true)}
+        >
+          <img
+            src={Bars}
+            alt=""
+            style={{ width: "1.5rem", height: "1.5rem" }}
+          />
+        </div>
+      ) : (
+        <ul className="header-menu">
+          <li>
+            <Link
+              onClick={() => setMenuOpend(false)}
+              to="home"
+              spy={true}
+              smooth={true}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpend(false)}
+              to="programs"
+              spy={true}
+              smooth={true}
+            >
+              Programs
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpend(false)}
+              to="reasons"
+              spy={true}
+              smooth={true}
+            >
+              Why us
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpend(false)}
+              to="plans"
+              spy={true}
+              smooth={true}
+            >
+              Plans
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpend(false)}
+              to="Testimonials"
+              spy={true}
+              smooth={true}
+            >
+              Testimonials
+            </Link>
+          </li>
         </ul>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,15 +6,26 @@ import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
 
+import {motion} from 'framer-motion'
+import NumberCounter from 'number-counter'
+
 const Hero = () => {
+  const transition = {type: 'spring', duration: 3}
+  const mobile = window.innerWidth <= 768 ? true : false;
+
   return (
-    <div className="hero">
+    <div className="hero" id="home">
       <div className="blur hero-blur"></div>
       <div className="left-h">
         <Header />
 
         <div className="the-best-ad">
-          <div></div>
+          <motion.div
+          initial={{left: mobile ? '165px' : '238px'}}
+          whileInView={{left: '8px'}}
+          transition={{...transition, type: 'tween'}}
+          >
+          </motion.div>
           <span>the best fitness club in the town</span>
         </div>
         {/* Hero Heading */}
@@ -37,15 +48,30 @@ const Hero = () => {
         {/* Figures*/}
         <div className="figures">
           <div>
-            <span>+140</span>
+            <span><NumberCounter
+            end={140}
+            start={100}
+            preFix="+"
+            delay="4"
+            /></span>
             <span>expert coachs</span>
           </div>
           <div>
-            <span>+978</span>
+            <span><NumberCounter
+            end={978}
+            start={820}
+            preFix="+"
+            delay="4"
+            /></span>
             <span>members joined</span>
           </div>
           <div>
-            <span>+50</span>
+            <span><NumberCounter
+            end={50}
+            start={20}
+            preFix="+"
+            delay="2"
+            /></span>
             <span>fitness programs</span>
           </div>
         </div>
@@ -60,24 +86,36 @@ const Hero = () => {
       <div className="right-h">
         <button className="btn">Join Now</button>
 
-        <div className="heart-rate">
+        <motion.div 
+        transition={transition}
+        initial={{right: '-1rem'}}
+        whileInView={{right: '4rem'}}
+        className="heart-rate">
           <img src={Heart} alt="" />
           <span>Heart Rate</span>
           <span>116 bpm</span>
-        </div>
+        </motion.div>
 
         {/* hero image */}
         <img src={hero_image} alt="" className="hero-image" />
-        <img src={hero_image_back} alt="" className="hero-image-back" />
+        <motion.img 
+        initial={{right: '11rem'}}
+        whileInView={{right: '20rem'}}
+        transition={transition}
+        src={hero_image_back} alt="" className="hero-image-back" />
 
         {/* calories */}
-        <div className="calories">
+        <motion.div 
+        initial={{right: '37rem'}}
+        whileInView={{right: '28rem'}}
+        transition={transition}
+        className="calories">
           <img src={Calories} alt="" />
           <div>
             <span>Calories Burned</span>
             <span>220 kcal</span>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
